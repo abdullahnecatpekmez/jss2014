8358fc374ef65bfc536425fd8cccbfb5d1b63634
abdullahnecatpekmez <abdullahnecatpekmez@hotmail.com>
2014-07-24 13:06:30 +0300

    add netbeans project

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -6,12 +6,52 @@
 
 package model;
 
+import cinema.Theater;
+
 /**
  *
  * @author abdullah
  */
 public class Ticket {
+    private int id;
     private int row;
     private int col;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public Hour getHour() {
+        return hour;
+    }
+
+    public Ticket(int id, int row, int col, Hour hour, Theater theater) {
+        this.id = id;
+        this.row = row;
+        this.col = col;
+        this.hour = hour;
+        this.theater = theater;
+    }
+    public Ticket( int row, int col, Hour hour, Theater theater) {
+        this.id = id;
+        this.row = row;
+        this.col = col;
+        this.hour = hour;
+        this.theater = theater;
+    }
+
+    public Theater getTheater() {
+        return theater;
+    }
+    private Hour hour;
+    private Theater theater;
     
 }
